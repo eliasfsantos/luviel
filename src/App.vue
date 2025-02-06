0b830f8a08b44a9d8dc2fccc06cc27605e9a768a
eliasfsantos <elias.formula@gmail.com>
2025-02-06 20:14:48 -0300

    fix:logo.svg

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/LOGO.svg" width="285" height="285" />
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="285" height="285" />
 
     <div class="wrapper">
       <HelloWorld msg=" " />
