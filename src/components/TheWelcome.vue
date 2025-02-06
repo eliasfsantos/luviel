db0391135d7f1462d75400fb4f8e77e629c26ec5
eliasfsantos <elias.formula@gmail.com>
2025-02-05 23:34:34 -0300

    First commmit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,33 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import InstagramIcon from './icons/IconInstagram.vue'
+import EmailIcon from './icons/IconEmail.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <InstagramIcon />
+    </template>
+    <template #heading>Instagram</template>
+
+    Veja a página do 
+    <a href="https://instagram.com/luvielengenharia" target="_blank" rel="noopener">instagram</a>
+    da Luviel Engenharia 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EmailIcon />
+    </template>
+    <template #heading>Email</template>
+
+    Entre em contato conosco através do nosso
+    <a href="mailto:contato@luviel.com.br" target="_blank" rel="noopener">E-mail</a>: 
+    <a href="mailto:contato@luviel.com.br" target="_blank" rel="noopener">contato@luviel.com.br</a>
+
+  </WelcomeItem>
+
+</template>
---
+++ src/components/icons/IconEmail.vue
@@ -0,0 +1,24 @@
+<template>
+   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor">
+      <g
+     id="Page-1"
+     stroke="none"
+     stroke-width="1"
+     fill="none"
+     fill-rule="evenodd">
+    <g
+       id="Dribbble-Light-Preview"
+       transform="translate(-300,-922)"
+       fill="currentColor">
+      <g
+         id="icons"
+         transform="translate(56,162)">
+        <path
+           d="m 262,764.291 -8,7.027 -8,-7.037 V 764 h 16 z M 246,775 v -8.055 l 8,7.035 8,-7.027 V 775 Z m -2,2 h 20 v -15 h -20 z"
+           id="email-[#1573]" />
+      </g>
+    </g>
+  </g>
+   </svg>
+ </template>
+ 
